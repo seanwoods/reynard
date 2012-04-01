261a707209c51bcaf15c8a491ebe204c3f2431c7
Sean Woods <sean@seanwoods.com>
2012-04-01 14:59:31 -0400

    Added logging for IP checks.

--- reynard/access.py
+++ reynard/access.py
@@ -6,7 +6,13 @@ import cherrypy
 from cherrypy import Tool
 
 def check_auth(allowed_ip):
-    if cherrypy.request.remote.ip not in allowed_ip:
+    ip = cherrypy.request.remote.ip
+
+    cherrypy.log.error_log.info("Checking IP %s." % ip)
+    
+    if ip not in allowed_ip:
+        cherrypy.log.error_log.error("IP %s denied." % ip)
+
         raise cherrypy.HTTPError(401)
 
 cherrypy.tools.auth = Tool('on_start_resource', check_auth)
