57f9247723fc1c7a2a12cc061f2aab31825b50de
Sean Woods <sean@seanwoods.com>
2012-04-01 15:22:23 -0400

    Changing hook for authorization tool.

--- reynard/access.py
+++ reynard/access.py
@@ -15,4 +15,4 @@ def check_auth(allowed_ip):
 
         raise cherrypy.HTTPError(401)
 
-cherrypy.tools.auth = Tool('on_start_resource', check_auth)
+cherrypy.tools.auth = Tool('before_request_body', check_auth)
