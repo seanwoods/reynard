65910ab9ca26c50a210b8412a130edd99105fc52
Sean Woods <sean@seanwoods.com>
2012-04-01 16:43:09 -0500

    Handle scenario where ZeroMQ may already be installed.  Also, a utility for process checking.

--- setup.py
+++ setup.py
@@ -1,5 +1,42 @@
 from setuptools import setup, find_packages
 
+def semantic_greater_than(v1, v2):
+    """Compare semantic version sequences."""
+    
+    if v1 == v2:
+        return 0
+
+    v1 = [int(i) for i in v1.split('.')]
+    v2 = [int(i) for i in v2.split('.')]
+
+    for idx, num in enumerate(v1):
+        if num > v2[idx]:
+            return 1
+
+    return -1
+
+install_requires = [
+    'CherryPy>=3.2.0',
+    'Mako>=0.6.2'
+]
+
+# Detect a proper version of PyZMQ
+# TODO: Find a way to do this through setuptools...
+
+HAS_ZMQ = False
+
+try:
+    import zmq
+    
+    if semantic_greater_than(zmq.__version__, '2.1.11') >= 0:
+        HAS_ZMQ = True
+
+except ImportError:
+    pass
+
+if not HAS_ZMQ:
+    install_requires.append('PyZMQ>=2.1.11')
+
 setup(
     name = 'Reynard',
     version = '0.2-alpha',
@@ -15,9 +52,7 @@ setup(
         ]
     },
     
-    install_requires = [
-        'CherryPy>=3.2.0',
-    ],
+    install_requires = install_requires,
     
     zip_safe = False
 
